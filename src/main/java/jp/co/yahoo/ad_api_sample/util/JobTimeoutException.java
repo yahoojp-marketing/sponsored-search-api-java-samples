ae106c5e8eddafbb44ad23598e916498d9ab42d9
Yahoo!マーケティングソリューション <yahoojp-marketingsolutions-github@mail.yahoo.co.jp>
2016-10-25 16:35:42 +0900

    Update

--- src/main/java/jp/co/yahoo/ad_api_sample/util/JobTimeoutException.java
+++ src/main/java/jp/co/yahoo/ad_api_sample/util/JobTimeoutException.java
@@ -1,8 +1,5 @@
 package jp.co.yahoo.ad_api_sample.util;
 
-/**
- * Created by ttakami on 2016/04/28.
- */
 public class JobTimeoutException extends RuntimeException {
 
   public JobTimeoutException(String message) {
